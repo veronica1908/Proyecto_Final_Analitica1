222bea6d21ffd750f4cdc8090ce373968312a1f3
veronica1908 <72422742+veronica1908@users.noreply.github.com>
2023-06-04 13:36:41 -0500

    Update app.py
    insertar titulo

--- app.py
+++ app.py
@@ -235,12 +235,10 @@ DESA = DESA.drop('EVENT START DATE', axis=1)
 """
 Equipo de trabajo: Aura Luz Moreno Díaz, Marcelo Lemus, Verónica Andrea Morales González"""
 """Semestre: 2023-2"""
-"""
 
-# Desastres en Canadá: Incidencia de los desastres en la cotidianidad, un énfasis sobre los incendios
+st.title(Desastres en Canadá: Incidencia de los desastres en la cotidianidad, un énfasis sobre los incendios)
 
 
-"""
 
 #Se revisa a nivel general, cómo es la distribución de la cantidad de desastres por cada tipo y cuál es el que tiene mayor ocurrencia en el periodo.
 

4645a85987361fb9e37541d82be7a29792d015ba
veronica1908 <72422742+veronica1908@users.noreply.github.com>
2023-06-04 15:34:13 -0500

    Update app.py
    ajustes imagenes y texto

--- app.py
+++ app.py
@@ -261,18 +261,18 @@ st.image(imageB, caption=None, width=100, use_column_width=None, clamp=False, ch
 
 st.markdown("<h1 style='text-align: center; color: #840901;'>Desastres en Canadá: Énfasis en los incendios, ¿Podemos hacer algo para controlarlos?</h1>", unsafe_allow_html=True)
 
-# AGREGAMOS UNA IMAGEN
-image= "incendiosCanada.jpg"
-
-st.image(image, caption="Tomado de: https://www.istockphoto.com/es/foto/incendios-de-lytton-bc-gm1326687341-411303646", width=None, use_column_width=150, clamp=False, channels="RGB", output_format="auto")
-
-
 #INICIAMOS EL DESARROLLO DE LAS PREGUNTAS, PRESENTÁNDOLO COMO RESPUESTAS INFORMATIVAS EN STREAMLIT
 
 #1
 ###
 #Se revisa a nivel general, cómo es la distribución de la cantidad de desastres por cada tipo y cuál es el que tiene mayor ocurrencia en el periodo.
-st.markdown("<h6 style='text-align: center; color: #525252;'>El desastre de mayor ocurrencia es el de inundaciones, en segundo lugar las tormentas y en tercer lugar los incendios, por lo tanto, hacer énfases en el tipo de desastres de incendios vale la pena, ya que está en el top 3 de ocurrencia, sin embargo, sería interesante indagar sobre algunos datos de las inundaciones y de las tormentas, aunque estos tipos de desastres, tienen menos posibilidades de ser controlados.</h6>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center; color: #525252;'>Existe una gran cantidad de tipos de desastres, cuando se mira por la cantidad de ocurrencias, se tiene que el desastre de mayor ocurrencia es el de inundaciones, en segundo lugar las tormentas y en tercer lugar los incendios, por lo tanto hacer énfases en estos tipos de desastres vale la pena.</h6>", unsafe_allow_html=True)
+
+desastre=DESA['EVENT TYPE'].value_counts()
+desastre_df = pd.DataFrame({'EVENT TYPE': desastre.index, 'Cantidad desastres': desastre.values})
+figd = px.bar(desastre_df, x='EVENT TYPE', y='Cantidad desastres', labels={'EVENT TYPE': 'Tipo de desastre', 'desastre_df': 'Tipo de desastre'})
+
+st.plotly_chart(figd)
 
 # AGREGAMOS UNA IMAGEN
 imageI= "inundaciones.jpg"
@@ -280,13 +280,6 @@ imageI= "inundaciones.jpg"
 st.image(imageI, caption="Tomado de: https://media.istockphoto.com/id/1356603199/es/foto/inundaci%C3%B3n-y-humo-negro-del-fuego-en-la-ciudad-y-las-tierras-de-cultivo-despu%C3%A9s-de-la-tormenta.jpg?s=612x612&w=0&k=20&c=Ep9sSC__XJVVePWa1eXCU7fyVLGjb8qBZVO2nuZ1mGc=", width=None, use_column_width=150, clamp=False, channels="RGB", output_format="auto")
 
 
-
-
-desastre=DESA['EVENT TYPE'].value_counts()
-desastre_df = pd.DataFrame({'EVENT TYPE': desastre.index, 'Cantidad desastres': desastre.values})
-figd = px.bar(desastre_df, x='EVENT TYPE', y='Cantidad desastres', labels={'EVENT TYPE': 'Tipo de desastre', 'desastre_df': 'Tipo de desastre'})
-
-st.plotly_chart(figd)
 
 #2
 st.markdown("<h2 style='text-align: center; color: #930000;'>Cantidad de muertes generadas por los tres principales tipos de desastre</h2>", unsafe_allow_html=True)
@@ -306,7 +299,6 @@ muertes = datos_filtrados.groupby('EVENT TYPE')['FATALITIES'].sum().reset_index(
 df_muertes = pd.DataFrame({'Tipo de evento': muertes['EVENT TYPE'], 'Cantidad de muertes': muertes['FATALITIES']})
 fig = px.bar(df_muertes, x='Tipo de evento', y='Cantidad de muertes',labels={'Tipo de evento': 'Tipo de evento', 'Cantidad de muertes': 'Cantidad de muertes'},title='Cantidad de muertes por tipo de evento')
 st.plotly_chart(fig)
-
 
 
 ###

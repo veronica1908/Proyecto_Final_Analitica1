799a4231eae0d6979b3666d232b236be20d61bad
veronica1908 <72422742+veronica1908@users.noreply.github.com>
2023-06-06 19:52:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -461,7 +461,7 @@ incendios_con_rociadores = conteo_performance['Sprinkler operated']
 total_incendios = ROC.shape[0]
 porcentaje_efectividad = (incendios_con_rociadores / total_incendios) * 100
 incendios_sin_rociadores = total_incendios - incendios_con_rociadores
-data = {'Resultado': ['Si funcionarón', 'No funcionarón'],'Cantidad': [incendios_con_rociadores, incendios_sin_rociadores]}
+data = {'Resultado': ['Si funcionaron', 'No funcionaron'],'Cantidad': [incendios_con_rociadores, incendios_sin_rociadores]}
 
 df = pd.DataFrame(data)
 
@@ -499,7 +499,7 @@ AH=AH.rename(columns={'Performance of smoke alarm device, residential fires':'pe
 conteo_performance = AH['performance_of_system'].value_counts()
 incendios_con_alarma = conteo_performance['Alarm activated']
 incendios_sin_alarma = total_incendios - incendios_con_alarma
-#data = {'Resultado': ['Alarmas Activadas', 'Alarmas No Activadas'],'Cantidad': [incendios_con_alarma, incendios_sin_alarma]}
+data = {'Resultado': ['Alarmas Activadas', 'Alarmas No Activadas'],'Cantidad': [incendios_con_alarma, incendios_sin_alarma]}
 
 df = pd.DataFrame(data)
 st.write(df)
@@ -524,7 +524,7 @@ imageAH= "alarmahumo.jpg"
 c15.image(imageAH, caption="Alarma de humo. Tomado de: https://media.istockphoto.com/id/1332514392/es/foto/detector-de-humo-y-alarma-contra-incendios-en-el-fondo-de-acci%C3%B3n.jpg?s=612x612&w=0&k=20&c=PRkBl_EFCHHO0LArDQE4cDs6CkCm_saix-YW9yyfmtE=", width=500, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 #enviar a streamlit
-c15.plotly_chart(figah)
+#c15.plotly_chart(figah)
 
 #miremos esta bellecita
 
